566ce78ea9ca0fbf51db262704fa71b59320fedd
Sean Bigelow <sean.bigelow1@gmail.com>
2020-11-05 18:30:26 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(string)
-  if string == string.downcase
+  if string == string.downcase()
     return "HUH?! SPEAK UP, SONNY!"
   elsif string == string.upcase()
     return "NO, NOT SINCE 1938!"
